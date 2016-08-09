e9e03c4d06ada9636e8beded46b427e4fdae6287
Said ID BAIH <said.idbaih@gmail.com>
2016-08-09 16:14:05 +0200

    Ex1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  # get 'tasks/index'
+  get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/new'
+  get 'tasks/new', to: 'tasks#new'
+
+  # get 'tasks/create'
+  post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/show'
+  get 'tasks/:id', to: 'tasks#show'
+
+  # get 'tasks/edit'
+  get 'tasks/:id/edit', to: "tasks#edit"
+
+  # get 'tasks/update'
+  patch 'tasks/:id', to: "tasks#update", as: :task
+
+  # get 'tasks/destroy'
+  delete 'tasks/:id', to: "tasks#destroy"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.new(description: "Desc1", author: "Aut1", priority: 1).save
+Task.new(description: "Desc2", author: "Aut2", priority: 2).save
+Task.new(description: "Desc3", author: "Aut3", priority: 3).save
