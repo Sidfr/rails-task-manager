4ba58bc9869f716cb81e4a853c0fbc22de6252e6
Said ID BAIH <said.idbaih@gmail.com>
2016-08-09 16:25:15 +0200

    Ex 1.1 : with partials and resources

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_find_param, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -38,5 +36,10 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:description, :author, :priority)
   end
+
+  def require_find_param
+    @task = Task.find(params[:id])
+  end
+
 
 end
